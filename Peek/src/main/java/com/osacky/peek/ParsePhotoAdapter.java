969efdbd760d857bc4ecc4e32bce8956ea269570
Nelson Osacky <nelson@osacky.com>
2013-11-10 15:28:53 -0600

    added double image to feed

--- Peek/src/main/java/com/osacky/peek/ParsePhotoAdapter.java
+++ Peek/src/main/java/com/osacky/peek/ParsePhotoAdapter.java
@@ -22,6 +22,7 @@ import com.parse.ParseFile;
 import com.parse.ParseQueryAdapter;
 
 import java.lang.reflect.Type;
+import java.util.HashMap;
 import java.util.Map;
 
 public class ParsePhotoAdapter extends ParseQueryAdapter<Photo> {
@@ -29,10 +30,15 @@ public class ParsePhotoAdapter extends ParseQueryAdapter<Photo> {
     private LayoutInflater layoutInflater;
     private SharedPreferences sharedPreferences;
 
+    private Map<String, Bitmap> topMap;
+    private Map<String, Bitmap> bottomMap;
+
     public ParsePhotoAdapter(Context context, QueryFactory queryFactory) {
         super(context, queryFactory);
         layoutInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        topMap = new HashMap<String, Bitmap>();
+        bottomMap = new HashMap<String, Bitmap>();
     }
 
     @Override
@@ -43,6 +49,7 @@ public class ParsePhotoAdapter extends ParseQueryAdapter<Photo> {
         final TextView textView = (TextView) v.findViewById(R.id.text1);
         final ImageView senderImage = (ImageView) v.findViewById(R.id.userprofile);
         final ImageView imageView = (ImageView) v.findViewById(R.id.icon);
+        final ImageView bottom = (ImageView)v.findViewById(R.id.second);
 
         String sender = object.getSender();
         textView.setText(sender);
@@ -57,19 +64,33 @@ public class ParsePhotoAdapter extends ParseQueryAdapter<Photo> {
                     senderImage.setImageURI(Uri.parse(contact.getPhotoURI()));
                 }
                 textView.setText(contact.getName());
-
             }
         }
 
-        ParseFile top = object.getTop();
-        if (top != null) {
-            top.getDataInBackground(new GetDataCallback() {
-                @Override
-                public void done(byte[] bytes, ParseException e) {
-                    Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                    imageView.setImageBitmap(bitmap);
-                }
-            });
+        if (topMap.containsKey(object.getTime()) && bottomMap.containsKey(object.getTime())) {
+            imageView.setImageBitmap(topMap.get(object.getTime()));
+            bottom.setImageBitmap(bottomMap.get(object.getTime()));
+        } else {
+            ParseFile top = object.getTop();
+            if (top != null) {
+                top.getDataInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] bytes, ParseException e) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        imageView.setImageBitmap(bitmap);
+                    }
+                });
+            }
+            ParseFile bot = object.getBottom();
+            if (bot != null) {
+                bot.getDataInBackground(new GetDataCallback() {
+                    @Override
+                    public void done(byte[] bytes, ParseException e) {
+                        Bitmap bitmap = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                        bottom.setImageBitmap(bitmap);
+                    }
+                });
+            }
         }
         return v;
     }
